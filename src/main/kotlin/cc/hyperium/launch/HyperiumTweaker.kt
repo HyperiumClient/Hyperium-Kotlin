0d0b69ff3c45b08a901e4fc0f6edd9b7ad055bb2
FalseHonesty <skipboman0@gmail.com>
2018-09-29 12:03:04 -0400

    clean up some junk

--- src/main/kotlin/cc/hyperium/launch/HyperiumTweaker.kt
+++ src/main/kotlin/cc/hyperium/launch/HyperiumTweaker.kt
@@ -1,29 +1,13 @@
 package cc.hyperium.launch
 
-import net.minecraft.launchwrapper.ITweaker
+import me.kbrewster.blazeapi.internal.launch.BlazeTweaker
 import net.minecraft.launchwrapper.LaunchClassLoader
-import org.spongepowered.asm.launch.MixinBootstrap
-import org.spongepowered.asm.mixin.MixinEnvironment
 import org.spongepowered.asm.mixin.Mixins
-import java.io.File
 
-class HyperiumTweaker : ITweaker {
-    override fun acceptOptions(args: MutableList<String>?, gameDir: File?, assetsDir: File?, profile: String?) {}
+class HyperiumTweaker : BlazeTweaker() {
+    override fun injectIntoClassLoader(cl: LaunchClassLoader) {
+        super.injectIntoClassLoader(cl)
 
-    override fun getLaunchTarget(): String {
-        return "net.minecraft.client.main.Main"
-    }
-
-    override fun injectIntoClassLoader(classLoader: LaunchClassLoader?) {
-        MixinBootstrap.init()
-
-        val environment = MixinEnvironment.getDefaultEnvironment()
         Mixins.addConfiguration("mixins.hyperium.json")
-        environment.obfuscationContext = "notch"
-        environment.side = MixinEnvironment.Side.CLIENT
-    }
-
-    override fun getLaunchArguments(): Array<String> {
-        return arrayOf("--version", "1.8.9", "--accessToken", "gey")
     }
 }
