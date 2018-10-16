a95830cc139bb1e23bc0acf907de4e594c87277c
feature <15078089+feature@users.noreply.github.com>
2018-10-17 00:26:08 +0200

    Add buildSrc and moved dependencies and versions into separate files

---
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -0,0 +1,25 @@
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.repositories
+
+object Dependencies {
+    const val blazeApi = "com.github.KevinPriv:BlazeAPI::${Versions.blazeApi}"
+
+    const val kryonet = "com.github.FalseHonesty:kryonet:${Versions.kryonet}"
+    const val reflections = "com.github.FalseHonesty:reflections:${Versions.reflections}"
+    const val hyperiumNetworking = "com.github.FalseHonesty:Hyperium-Networking:${Versions.hyperiumNetworking}"
+}
+
+fun Project.applyStandardDependencies() {
+
+    dependencies {
+        "compile"(kotlin("stdlib-jdk8"))
+
+        "compile"(Dependencies.blazeApi)
+
+        "compile"(Dependencies.kryonet)
+        "compile"(Dependencies.reflections)
+        "compile"(Dependencies.hyperiumNetworking)
+    }
+}
---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+    const val kotlin = "1.2.71"
+
+    // Libraries
+
+    const val blazeApi = "minecraft-1.8.9-SNAPSHOT"
+    const val reflections = "855f40b80b"
+    const val kryonet = "2.22"
+    const val hyperiumNetworking = "66a8dba092"
+}
