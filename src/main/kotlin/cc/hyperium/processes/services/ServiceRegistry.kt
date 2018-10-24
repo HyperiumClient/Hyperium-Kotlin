426f25e03d2ec90c35b8f144bacc74683858de39
KevinPriv <kb215@ga.newcastle.sch.uk>
2018-10-24 17:22:22 +0100

    ServiceRegistry is now a Registry for AbstractServices rather than a generalised Process

--- src/main/kotlin/cc/hyperium/processes/services/ServiceRegistry.kt
+++ src/main/kotlin/cc/hyperium/processes/services/ServiceRegistry.kt
@@ -1,12 +1,11 @@
 package cc.hyperium.processes.services
 
 import cc.hyperium.Hyperium
-import cc.hyperium.processes.Process
 import cc.hyperium.utils.Registry
 import net.minecraft.client.resources.I18n
 import org.reflections.Reflections
 
-class ServiceRegistry : Registry<Process>() {
+class ServiceRegistry : Registry<AbstractService>() {
 
     private var initialised = false
 
@@ -15,35 +14,33 @@ class ServiceRegistry : Registry<Process>() {
             throw IllegalStateException("ServiceRegistry has already been initialised!")
         }
 
-        fun onError(serviceName: String): Process? {
+        fun onError(serviceName: String): AbstractService? {
             Hyperium.LOGGER.error(I18n.format("error.loading.service", serviceName))
             return null
         }
 
         this += ref.getTypesAnnotatedWith(Service::class.java)
                 .asSequence()
-                .map { it.kotlin.objectInstance as? Process ?: onError(it.name) }
+                .map { it.kotlin.objectInstance as? AbstractService ?: onError(it.name) }
                 .filterNotNull()
                 .toList()
 
         this.initialised = true
     }
 
-    override fun add(element: Process): Boolean {
+    override fun add(element: AbstractService): Boolean {
         element.initialize()
         return super.add(element)
     }
 
-    override fun addAll(elements: Collection<Process>): Boolean {
-        elements.forEach(Process::initialize)
+    override fun addAll(elements: Collection<AbstractService>): Boolean {
+        elements.forEach(AbstractService::initialize)
         return super.addAll(elements)
     }
 
 
     fun shutdownServices() {
-        this.removeAll { service ->
-            service.kill()
-        }
+        this.removeAll(AbstractService::kill)
     }
 
 }
