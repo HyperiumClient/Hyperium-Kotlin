12b9bc29bf878778786a7caa2688cca4e9712de2
FalseHonesty <skipboman0@gmail.com>
2018-11-24 09:08:01 -0500

    Refactored

--- src/main/kotlin/cc/hyperium/utils/ResourceManager.kt
+++ src/main/kotlin/cc/hyperium/utils/ResourceManager.kt
@@ -1,9 +1,9 @@
 package cc.hyperium.utils
 
+import cc.hyperium.processes.services.AbstractService
+import cc.hyperium.processes.services.Service
 import net.minecraft.util.ResourceLocation
+import org.kodein.di.Kodein
 
-class ResourceManager : HashMap<String, ResourceLocation>() {
-    fun get(k: String, def: ResourceLocation?) : ResourceLocation {
-        return computeIfAbsent(k){ def!! }
-    }
-}
+@Service
+class ResourceManager(override val kodein: Kodein) : AbstractService(), MutableMap<String, ResourceLocation> by HashMap()
